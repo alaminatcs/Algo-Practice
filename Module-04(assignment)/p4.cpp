6ae4f8715ef61d8aca746b59cbe58df0cd9534aa
alaminnhasan <alaminedu.cse@gmail.com>
2023-10-13 08:18:07 +0600

    Rename

--- Module-04(assignment)/p4.cpp
+++ Module-04(assignment)/p4.cpp
@@ -1,29 +1,34 @@
 // time complexity analysis
+/*
+a)
+int count = 0;
+for (int i = n; i > 0; i /= 2)
+{
+    for (int j = 0; j < n; j += 5)
+    {
+        count += 1;
+    }
+}
 
-// int count = 0;
-// for (int i = n; i > 0; i /= 2)
-// {
-//     for (int j = 0; j < n; j += 5)
-//     {
-//         count += 1;
-//     }
-// }
+b)
+for (int i = 1; i * i < n; i++)
+{
+    cout << “hello”;
+}
 
-// for (int i = 1; i * i < n; i++)
-// {
-//     cout << “hello”;
-// }
+c)
+for (int i = 1; i < n; i = i * 2)
+{
+    for (int j = 1; j * j < n; j += 2)
+    {
+        cout << “hello”;
+    }
+}
 
-// for (int i = 1; i < n; i = i * 2)
-// {
-//     for (int j = 1; j * j < n; j += 2)
-//     {
-//         cout << “hello”;
-//     }
-// }
-
-// int m = 1;
-// for (int i = 0; m <= n; i++)
-// {
-//     m += i;
-// }
+d)
+int m = 1;
+for (int i = 0; m <= n; i++)
+{
+    m += i;
+}
+*/
